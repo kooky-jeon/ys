6369da1b94e1ac473a75ca14c31daa46ae09dea1
kooky-jeon <kookyjeon@gmail.com>
2026-05-06 14:00:31 +0900

    Add files via upload

--- my-dashboard/api/analyze.js
+++ my-dashboard/api/analyze.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -26,4 +26,4 @@ export default async function handler(req, res) {
   } catch (err) {
     res.status(500).json({ error: 'Internal server error', detail: err.message });
   }
-}
+};
